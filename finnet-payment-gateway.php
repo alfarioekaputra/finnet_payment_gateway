5d95bb8f71a16227a82a0d1d038c8d46599dad13
alfarioekaputra <alfarioekaputra@gmail.com>
2018-01-18 14:20:48 +0700

    change text for function

--- finnet-payment-gateway.php
+++ finnet-payment-gateway.php
@@ -7,8 +7,8 @@ Description: WooCommerce custom payment gateway integration on finnet.
 Version: 1.5.3
 */
 
-add_action( 'plugins_loaded', 'cwof_finnet_payment_gateway', 0 );
-function cwof_finnet_payment_gateway() {
+add_action( 'plugins_loaded', 'finnet_payment_gateway', 0 );
+function finnet_payment_gateway() {
     //if condition use to do nothin while WooCommerce is not installed
 	if ( ! class_exists( 'WC_Payment_Gateway' ) ) return;
 	include_once( 'finnet-payment-gateway-kd-bayar.php' );
@@ -16,8 +16,8 @@ function cwof_finnet_payment_gateway() {
 	include_once('finnet-payment-gateway-tcash.php');
 	include_once('finnet-payment-gateway-cc.php');
 	// class add it too WooCommerce
-	add_filter( 'woocommerce_payment_gateways', 'cwof_add_finnet_payment_gateway' );
-	function cwof_add_finnet_payment_gateway( $methods ) {
+	add_filter( 'woocommerce_payment_gateways', 'add_finnet_payment_gateway' );
+	function add_finnet_payment_gateway( $methods ) {
 		$methods[] = 'finnet_payment_kd_bayar';
 		$methods[] = 'finnet_payment_VA';
 		$methods[] = 'finnet_payment_TCash';
@@ -26,10 +26,10 @@ function cwof_finnet_payment_gateway() {
 	}
 }
 // Add custom action links
-add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'cwof_finnet_payment_gateway_action_links' );
-function cwof_finnet_payment_gateway_action_links( $links ) {
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'finnet_payment_gateway_action_links' );
+function finnet_payment_gateway_action_links( $links ) {
 	$plugin_links = array(
-		'<a href="' . admin_url( 'admin.php?page=wc-settings&tab=checkout' ) . '">' . __( 'Settings', 'cwof-finnet-payment-gateway' ) . '</a>',
+		'<a href="' . admin_url( 'admin.php?page=wc-settings&tab=checkout' ) . '">' . __( 'Settings', 'finnet-payment-gateway' ) . '</a>',
 	);
 	return array_merge( $plugin_links, $links );
 }
